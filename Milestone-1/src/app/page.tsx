b54438413d23bb96dfd999246e6ea8bb9db4bbd1
Ahsanhashimofficial <ahsanhashim216@gmail.com>
2024-12-13 01:09:42 +0500

    hello world

---
+++ Milestone-1/src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function firstProject() {
+  return <div>
+    <h1 className="text-bold">Hello World</h1>
+    <p>
+      I am excited to share that I have successfully completed my first milestone in this React project! This milestone involved setting up the development environment, creating a basic React application, and adding a simple "Hello World" message. I also got hands-on experience with Node.js and React, gaining insights into their core functionalities. With this foundation in place, I am now looking forward to the upcoming milestones and continuing my journey in building interactive and dynamic web    applications.                            
+      <br/><br/>
+      <strong> Muneeba Arshad</strong>
+    </p>
+  </div>
+}
